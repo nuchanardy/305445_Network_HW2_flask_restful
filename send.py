e6c04fa6bb8cd89ecec015b61e5e02ac9cb69764
Nuchanard Yothapukdee <mushroom_nuch@hotmail.com>
2018-02-01 22:59:41 +0700

    add file send.py

---
+++ send.py
@@ -0,0 +1,26 @@
+from flask import Flask , request
+from flask_restful import Resource ,Api,reqparse
+import json,time
+from werkzeug.datastructures import FileStorage
+
+app = Flask (__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('image', type = FileStorage, location = 'files')
+
+class Send_image(Resource):
+    def get(self):
+        return {"message":"Plese sent 'image' (POST method) to me."}
+    def post(self):
+        args = parser.parse_args()
+        image = args['image']
+        image_name = image.filename
+        image.save(image_name)
+        return {"code": 200, "desc": "Upload success"}
+
+api.add_resource(Send_image,'/image')
+
+if __name__ == '__main__':
+        app.run(host = '0.0.0.0', port = 5500)
+
